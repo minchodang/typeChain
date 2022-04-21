3d7370c81458525e5f546bd07f6514f8a9313836
minsuKang <minsu910725@gmail.com>
2022-04-21 10:30:38 +0900

    Add: types 설정

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: String, age: Number, gender: String): string => {
+  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+};
+
+sayHi('minsu', 23, 'male');
+
+export {};
